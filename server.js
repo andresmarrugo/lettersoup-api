d7675b1da30ac88353d597536e19c8564262dd02
Andrés Marrugo <andrexmarrugo@gmail.com>
2024-07-16 08:55:31 -0500

    fix/ cors was added

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { getRandomWords } = require('./utils');
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors())
 
 app.get('/api/random-words', async (req, res) => {
   const minLength = parseInt(req.query.minLength, 10) || 0;
